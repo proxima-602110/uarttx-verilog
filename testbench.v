3186b1a7b79f4950abfc9b54a75be03f9d704525
proxima-602110 <venkat.rohan06@gmail.com>
2026-06-09 17:49:20 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -4,19 +4,19 @@ reg n_rst,clk,tx_en;
 reg[7:0] data;
 wire tx_out;
 
-uart_rtl dut1(.n_rst(n_rst),.clk(clk),.tx_en(tx_en),.data(data),.tx_out(tx_out));
+  uart_rtl dut1(.n_rst(n_rst),.clk(clk),.tx_en(tx_en),.data(data),.tx_out(tx_out)); //Instantiate the main transmitter module
 
-always #18.5185 begin clk=~clk;
+always #18.5185 begin clk=~clk; //ON Duration = Cycle Duration/2 = 18.5185 ns
 end
 
 initial begin
-//Reset state
+//Reset test
 n_rst=1'b0;
 clk=1'b0;
 tx_en=1'b0;
 data=8'd0;
 #10;
-//Reset disabled
+//Transmission test
 n_rst=1'b1; #18.5185
 data=8'b01100101; #80
 tx_en=1'b1; #40;
